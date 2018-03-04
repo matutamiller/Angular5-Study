104d9872e39768b453f3b366ddca99607559696d
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-03-04 16:25:29 +0100

    Add files via upload

---
+++ property binding/src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+  <h2> Welcome, {{name}}!</h2>
+  <input [id]="myId" type="text" value="Matie">
+  <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Matie">
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+  public name = "Matie";
+  public siteUrl = window.location.href;
+  public myId = "testId";
+  public isDisabled = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  greetUser() {
+    return "Hello " + this.name;
+  }
+}
