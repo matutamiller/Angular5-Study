ada36ba0f4aa7ef725b7492f9f8495c77bc9f3c7
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-03-06 18:13:04 +0100

    got acquainted with template reference variables

---
+++ template reference variables/src/app/test/test.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+  <h2> Welcome, {{name}}!</h2>
+  <input #myInput type="text">
+  <button (click) = "logMessage(myInput.value)">Log</button>
+  `,
+  styles: [`
+   
+  
+  `]
+})
+export class TestComponent implements OnInit {
+  public name = "Matie";
+  
+  
+ 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  logMessage(value) {
+    console.log(value);
+  }
+}
+
+
