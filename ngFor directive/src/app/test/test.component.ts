778f9e1f26d0ce5f7e6337117d041ecb8cc4979c
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-03-08 15:06:36 +0100

    studied ngFor directive

---
+++ ngFor directive/src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+  <div *ngFor = "let color of colors; index as i">
+    <h2>{{i}} {{color}}</h2>
+  </div>
+  `,
+  styles: [``]
+})
+export class TestComponent implements OnInit {
+
+  public colors = ["red", "green", "blue", "yellow"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
