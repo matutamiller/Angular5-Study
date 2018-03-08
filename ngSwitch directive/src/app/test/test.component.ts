727da8a08258856e13f4d739e71d7cfc886d54d7
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-03-08 14:50:20 +0100

    continued working with structural directives

---
+++ ngSwitch directive/src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+  <div [ngSwitch]="color">
+    <div *ngSwitchCase="'red'">You picked red color</div>
+    <div *ngSwitchCase="'green'">You picked green color</div>
+    <div *ngSwitchCase="'blue'">You picked blue color</div>
+    <div *ngSwitchDefault>Pick again</div>
+  </div>
+  `,
+  styles: [``]
+})
+export class TestComponent implements OnInit {
+
+  public color = "blue";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
